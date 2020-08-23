2c34e56df87940e33b685addb3af6a7ab9ce7544
manojsingh047 <manojsingh047@gmail.com>
2020-08-23 18:28:03 +0530

    gql get posts and post

--- reddit-server/src/entities/Post.ts
+++ reddit-server/src/entities/Post.ts
@@ -1,16 +1,22 @@
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { ObjectType, Field } from "type-graphql";
 
-@Entity()
+@ObjectType() //to decorate as a graphql entity
+@Entity() //to decorate as mikroORM entity
 export class Post {
+  @Field() //to decorate as gql field
   @PrimaryKey()
   id!: number;
 
-  @Property({ type: "date" })
+  @Field(() => String)
+  @Property({ type: "date" }) //to decorate as db field
   createdAt = new Date();
 
+  // @Field(() => String) //commneted because i dont want graphql to expose this field
   @Property({ type: "date", onUpdate: () => new Date() })
   updatedAt = new Date();
 
+  @Field()
   @Property({ type: "text" })
   title!: string;
 }
