b917afbcc7c5f9eb9a0f095f92122b0032018a81
Manoj Singh <manojsingh047@gmail.com>
2020-09-24 18:51:47 +0530

    custom cache for urql

--- reddit-client/src/pages/_app.tsx
+++ reddit-client/src/pages/_app.tsx
@@ -1,10 +1,53 @@
 import { ThemeProvider, CSSReset, ColorModeProvider } from '@chakra-ui/core'
 import theme from '../theme'
-import { createClient, Provider } from 'urql';
+import { createClient, dedupExchange, fetchExchange, Provider } from 'urql';
+import { Cache, cacheExchange, QueryInput } from '@urql/exchange-graphcache';
+import { LoginMutation, MeDocument, MeQuery, RegisterMutation } from '../generated/graphql';
+
+//to get proper types for our operations
+function betterUpdateQuery<Result, Query>(cache: Cache, qi: QueryInput, result: any, fn: (r: Result, q: Query) => Query) {
+  cache.updateQuery(qi, data => fn(result, data as any) as any);
+}
 
 function MyApp({ Component, pageProps }) {
   const client = createClient({
     url: 'http://localhost:4000/graphql',
+    exchanges: [dedupExchange, cacheExchange({
+      updates: {
+        Mutation: {
+          login: (_result, args, cache, info) => {
+            betterUpdateQuery<LoginMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.login.errors) {
+                  return query;
+                }
+                return {
+                  me: result.login.user
+                }
+              }
+            )
+          },
+          register: (_result, args, cache, info) => {
+            betterUpdateQuery<RegisterMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.register.errors) {
+                  return query;
+                }
+                return {
+                  me: result.register.user
+                }
+              }
+            )
+          },
+        },
+      },
+    }), fetchExchange],
     fetchOptions: {
       credentials: 'include'
     }
